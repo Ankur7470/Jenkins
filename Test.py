464f77f7a0af303d3bfed070221f9daf815a31d6
Ankur Sinha <86646635+Ankur7470@users.noreply.github.com>
2025-01-30 23:51:38 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -7,7 +7,7 @@ from Prog1 import summation
 class TestSum(unittest.TestCase):
     def test_list_int(self):
         """
-        Test case to add two numbers
+        Test case to added two numbers
         """
         data = [20, 36]
         result = summation(data)
